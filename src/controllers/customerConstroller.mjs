740104276ecc9034e09628900b94fa4de0f085da
OmarP-hub <omy250@gmail.com>
2024-05-29 19:35:45 -0700

    Fix: Error arreglado en registro de usuarios

--- src/controllers/customerConstroller.mjs
+++ src/controllers/customerConstroller.mjs
@@ -1,59 +1,54 @@
 const controller = {};
 
 controller.login = (req, res) => {
-    const { email, password } = req.body;
-    req.getConnection((err, connection) => {
-        if (err) return res.status(500).send(err);
+  const { email, password } = req.body;
+  req.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
 
-        connection.query('SELECT * FROM usuarios WHERE correo = ?', [email], (err, results) => {
-            if (err) return res.status(501).send(err);
-            if (results.length === 0) {
-                return res.status(404).send('Email not found');
-            }
+    connection.query('SELECT * FROM usuarios WHERE correo = ?', [email], (err, results) => {
+      if (err) return res.status(501).send(err);
+      if (results.length === 0) {
+        return res.status(404).send('Email not found');
+      }
 
-            const user = results[0];
+      const user = results[0];
 
-            console.log(user.pass);
-            console.log(password);
-            if (password === user.pass) {
-                // Si el correo y la contraseña coinciden, puedes iniciar la sesión del usuario aquí
-                res.redirect('/dashboard');
-            } else {
-                res.status(401).send('Incorrect password');
-            }
-            
-        });
+      console.log(user.pass);
+      console.log(password);
+      if (password === user.pass) {
+        // Si el correo y la contraseña coinciden, puedes iniciar la sesión del usuario aquí
+        res.redirect('/dashboard');
+      } else {
+        res.status(401).send('Incorrect password');
+      }
+
     });
+  });
 };
 
 
 controller.register = (req, res) => {
-    const { nombre, correo, pass } = req.body;
+  const { nombre, correo, pass } = req.body;
 
-    // Encriptar la contraseña antes de almacenarla
-    bcrypt.hash(contrasena, 10, (err, hashedPassword) => {
-        if (err) return res.status(500).send(err);
+  req.getConnection((err, connection) => {
+    if (err) return res.status(500).send(err);
 
-        req.getConnection((err, connection) => {
-            if (err) return res.status(500).send(err);
+    const newBeneficiary = {
+      nombre,
+      correo,
+      pass
+    };
 
-            const newBeneficiary = {
-                nombre,
-                correo,
-                pass
-            };
-
-            connection.query('INSERT INTO usuarios SET ?', newBeneficiary, (err, results) => {
-                if (err) return res.status(500).send(err);
-                res.send('Beneficiario agregado exitosamente');
-            });
-        });
+    connection.query('INSERT INTO usuarios SET ?', newBeneficiary, (err, results) => {
+      if (err) return res.status(500).send(err);
+      res.send('Beneficiario agregado exitosamente');
     });
+  });
 };
 
 controller.save = (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+  console.log(req.body);
+  res.send(req.body);
 };
 
 export default controller;
